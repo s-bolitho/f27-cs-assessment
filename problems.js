6d5fc6314d2c70cfdc5902baa0d74028628d032f
s-bolitho <sambolitho4@gmail.com>
2022-09-14 12:02:20 -0500

    Final Commit

--- problems.js
+++ problems.js
@@ -1,44 +1,71 @@
-let array = [1, 2, 3, 4, 5, -5]
-
-const sumZero = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        for (j = 0; j < array.length; j++) {
-            if (array[i] + array[j] === 0) { 
+const sumZero = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] + arr[j] === 0) {
                 return true
-            } else {
+            }
+        }
+    }
+    return false
+}
+
+console.log(sumZero([1, 2, 3, 4, 5, -4]))
+
+// O(n^2)
+
+const hasUniqueChars = (str) => {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j && str[i] === str[j]) {
                 return false
             }
         }
     }
+    return true
 }
 
-console.log(sumZero(array))
+console.log(hasUniqueChars('Monday'))
 
-// O(n)
-
-function makeUnique(str) {
-    let uniqueStr = ''
-
-    for (let i = 0; i < str.length; i++) {
-        if (uniqueStr.includes(str[i])) {
-            continue
-        } else {
-            uniqueStr += str[i]
-        }
-    }
-    return uniqueStr
-}
-
-console.log(makeUnique('moonday'))
-
-//O(1)
+//O(n^2)
 
 const isPangram = (str) => {
-    let strArr = string.toLowerCase();
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const allLettersObj = {
+        a:0,
+        b:0,
+        c:0,
+        d:0,
+        e:0,
+        f:0,
+        g:0,
+        h:0,
+        i:0,
+        j:0,
+        k:0,
+        l:0,
+        m:0,
+        n:0,
+        o:0,
+        p:0,
+        q:0,
+        r:0,
+        s:0,
+        t:0,
+        u:0,
+        v:0,
+        w:0,
+        x:0,
+        y:0,
+        z:0,
+    }
 
-    for (let i = 0; i < alphabet.length; i++) {
-        if (strArr.indexOf(alphabet[i]) < 0) {
+    for (let i = 0; i < str.lenght; i++) {
+        let letter = str[i]
+        letter = letter.toLowerCase()
+        allLettersObj[letter]++
+    }
+
+    for (let key in allLettersObj) {
+        if (allLettersObj[key] === 0) {
             return false
         }
     }
@@ -49,17 +76,17 @@ console.log(isPangram('The quick brown fox jumps over the lazy dog'))
 
 //O(n)
 
-function findLongestWord(str) {
-    var strSplit = str.split(' ');
-    var longestWord = 0;
-    for(var i = 0; i < strSplit.length; i++){
-      if(strSplit[i].length > longestWord){
-      longestWord = strSplit[i].length;
-       }
-    }
-    return longestWord;
-  }
-  
-  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+const findLongestWord = (listOfWords) => {
+    let winner = 0
 
-  //O(n^2)
+    for (let i = 0; i < listOfWords.length; i++) {
+        if (listOfWords[i].length > winner) {
+            winner = listOfWords[i].length
+        }
+    }
+    return winner
+}
+
+console.log(findLongestWord(['hi', 'hello']))
+
+//O(n)
